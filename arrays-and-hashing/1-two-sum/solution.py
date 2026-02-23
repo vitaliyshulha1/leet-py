6629b60541d4d1c378b405539f4d07ab4c17a77c
shulha <vitaliy.shulha@coeo-inkasso.de>
2026-02-23 11:20:23 +0100

    add comments

--- arrays-and-hashing/1-two-sum/solution.py
+++ arrays-and-hashing/1-two-sum/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}  # Этот блок должен быть внутри функции (сдвинут вправо)
+        seen = {} # we save all items with their index
         for i, n in enumerate(nums):
-            diff = target - n
-            if diff in seen:
+            diff = target - n # substract num from target so we get second number
+            if diff in seen: # check if the diff number is in already seen, if yes, return senn[index of diff] and i - current num
                 return [seen[diff], i]
-            seen[n] = i
+            seen[n] = i # just add curret number to seen
         return []
 
 sol = Solution()
