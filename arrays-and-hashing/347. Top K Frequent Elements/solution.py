8052a02b4c2693b1ca1b7d987254f5ac3e90208e
shulha <vitaliy.shulha@coeo-inkasso.de>
2026-03-17 16:20:09 +0100

    add top K 347

---
+++ arrays-and-hashing/347. Top K Frequent Elements/solution.py
@@ -0,0 +1,17 @@
+# 1. Сначала объявляем класс (шаблон с LeetCode)
+from typing import List
+from collections import Counter
+
+
+class Solution:
+  def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+    count = Counter(nums)
+    return [x for x, _ in count.most_common(k)]
+
+# 2. И только в самом конце создаем объект и вызываем метод
+sol = Solution()
+print(sol.topKFrequent([1,2,2,3,3,3], 2))
+
+
+#  вначале надо создать Dictionary где ключ цифра, а значение частота с какой число встречается . на пайтоне Counter считает частоты
+#  потом сортируем все объекты в Dictionary по значению и возварщаем то количесто которе нужно . most_common(k) воваршает топ К
